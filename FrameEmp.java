e8f60685ab4117534b0f0db606343e0faf38d511
Irineu Silva <142424957+cc23136@users.noreply.github.com>
2024-11-27 23:12:51 -0300

    Update FrameEmp.java

--- FrameEmp.java
+++ FrameEmp.java
@@ -1,1 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
 
+public class FrameDev extends JFrame {
+    private static int idBiblioteca, idB;
+    protected static Connection conexaoDados;
+
+    private static JTextField testeTxt;
+    private static JLabel testeL;
+    private static JButton testeB;
+
+    public FrameDev(int idBiblioteca) {
+        idB = idBiblioteca;
+        setTitle("Empréstimos" + idBiblioteca);
+        setSize(600, 400);
+        setLayout(new BorderLayout());
+
+        testeTxt = new JTextField();
+        testeL = new JTextField();
+        testeB = new JTextField();
+    }
+}
